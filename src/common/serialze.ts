1e6f7b5d6cfa9f821ba4ec129bf80bf7f170b5cd
Kody King <kody.j.king@gmail.com>
2019-08-22 23:41:23 -0700

    Added serial clone function.

--- src/common/serialze.ts
+++ src/common/serialze.ts
@@ -1,25 +1,29 @@
 import { isValueType } from "./clone";
-import Vector from "./Vector";
 
-const idToType = [] as Function[]
-const _typeToId = new Map<Function, number>()
-let idCounter = 0
+const namespace = new Map<string, any>()
 
-function typeToId( o: object ) {
-    let id = _typeToId.get( o.constructor )
-    if ( id === undefined ) {
-        id = idCounter++
-        _typeToId.set( o.constructor, id )
-        idToType[ id ] = o.constructor
-    }
-    return id
+export function serialize( object, space?: number ) {
+    return JSON.stringify( object, ( key, value ) => {
+        if ( isValueType( value ) ) return value
+        if ( value.constructor == undefined || value.constructor == Object ) return value
+        let $type = value.constructor.name
+        namespace.set( $type, value.constructor )
+        return Object.assign( { $type }, value )
+    }, space )
 }
 
-export function serialize( object ) {
-    JSON.stringify( object, ( key, value ) => {
-        if ( isValueType( value ) ) return value
-        if ( value.constructor == undefined ) return value
-        let $typeId = typeToId( value )
-        return Object.assign( { $typeId }, value )
+export function deserialize( object ) {
+    return JSON.parse( object, ( key, value ) => {
+        if ( value.$type === undefined )
+            return value
+        let type = namespace.get( value.$type )
+        delete value.$type
+        let instance = new type()
+        Object.assign( instance, value )
+        return instance
     } )
+}
+
+export function serialClone( object ) {
+    return deserialize( serialize( object ) )
 }
