5c00a53c2bd14901c4cc35bb588b6ee1af2db2e0
kjk <kody.j.king@gmail.com>
2021-08-02 01:03:15 -0700

    minor tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require( "path" )
-const webpack = require( "webpack" )
-const webroot = path.join( process.cwd(), "./www" )
+// const webpack = require("webpack")
+const path = require("path")
+const webroot = path.join(process.cwd(), "./www")
 module.exports = {
     entry: "./lib/index.js",
     output: {
@@ -10,17 +10,7 @@ module.exports = {
     devServer: {
         contentBase: webroot,
         hot: true,
-        host: "0.0.0.0",
-        port: 8080,
-        // proxy: [ {
-        //     path: "/api/**",
-        //     target: "http://localhost:8080",
-        //     secure: false
-        // } ]
-    },
-
-    // plugins: [
-    //     // new webpack.IgnorePlugin( /^crypto$/ ),
-    //     new webpack.HotModuleReplacementPlugin()
-    // ]
+        host: "localhost",
+        port: 8080
+    }
 }
