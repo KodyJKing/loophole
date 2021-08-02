9c4d4784e1ae73c325efe059905acc0d7b58a86d
kjk <kody.j.king@gmail.com>
2021-08-02 00:50:04 -0700

    updated webpack version

--- src/tiles/loadTileset.ts
+++ src/tiles/loadTileset.ts
@@ -1,11 +1,15 @@
-import path from "path"
 import { getImage } from "geode/lib/assets"
 import Tile from "./Tile"
+
+function basename( path: string ) {
+    let parts = path.split( /[\\\/.]/g )
+    return parts[ parts.length - 2 ]
+}
 
 export default function loadTileset( tileSet: TiledTileSet ) {
     let result: Tile[] = []
     for ( let tile of tileSet.tiles ) {
-        let imageFile = path.basename( tile.image )
+        let imageFile = basename( tile.image )
         let name = imageFile.split( "." )[ 0 ]
         let image = getImage( imageFile )
         result.push( new Tile( name, result.length ) )
